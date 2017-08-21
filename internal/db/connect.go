5dd64010797520cfd8fc5d877df84463c99fc9b6
Gorm Casper <g@g.dk>
2017-08-21 22:32:30 +0200

    some refactoring

--- internal/db/connect.go
+++ internal/db/connect.go
@@ -2,36 +2,23 @@ package db
 
 import (
 	"fmt"
-	"os"
 
 	_ "github.com/casperin/jazz_reader/internal/configuration"
-	"github.com/casperin/jazz_reader/internal/util/str"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
-var (
-	base       = "user=%s password=%s dbname=%s sslmode=disable"
-	dbUser     = viper.GetString("db_user")
-	dbPass     = viper.GetString("db_pass")
-	dbName     = str.Or(viper.GetString("db_name"), "jazzreader_dev")
-	testDbUser = viper.GetString("test_db_user")
-	testDbPass = viper.GetString("test_db_pass")
-	testDbName = str.Or(viper.GetString("test_db_name"), "jazzreader_test")
-	Conn       *sqlx.DB
-)
+var Conn *sqlx.DB
 
 func init() {
-	connect()
-}
-
-func connect() {
-	var cs string
-	if os.Getenv("ENV") == "TEST" {
-		cs = fmt.Sprintf(base, testDbUser, testDbPass, testDbName)
-	} else {
-		cs = fmt.Sprintf(base, dbUser, dbPass, dbName)
-	}
-	Conn = sqlx.MustConnect("postgres", cs)
+	Conn = sqlx.MustConnect(
+		"postgres",
+		fmt.Sprintf(
+			"user=%s password=%s dbname=%s sslmode=disable",
+			viper.GetString("db_user"),
+			viper.GetString("db_pass"),
+			viper.GetString("db_name"),
+		),
+	)
 }
